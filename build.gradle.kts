c27bfddf95c1049e4af5a16f3a9378462f8516d4
rcan <rcaninhu@gmail.com>
2024-03-25 15:03:34 +0900

    jdk17 -> jdk21 kotlin 1.8.0 -> 1.9.22 springboot 3.0.4 -> 3.2.4 spring cloud 2022.0.1 -> 2023.0.0
    docker build
    gradle:jdk17 -> amazoncorretto:21-alpine
    
    등등 변경.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,27 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.0.4"
-    id("io.spring.dependency-management") version "1.1.0"
-    id("org.owasp.dependencycheck") version "7.3.2"
-    id("com.bmuschko.docker-spring-boot-application") version "9.2.1"
-    kotlin("jvm") version "1.8.0"
-    kotlin("plugin.spring") version "1.8.0"
-    kotlin("plugin.jpa") version "1.8.0"
+    id("org.springframework.boot") version "3.2.4"
+    id("io.spring.dependency-management") version "1.1.4"
+    // https://plugins.gradle.org/plugin/org.owasp.dependencycheck
+    id("org.owasp.dependencycheck") version "9.0.10"
+    // https://plugins.gradle.org/plugin/com.bmuschko.docker-spring-boot-application
+    id("com.bmuschko.docker-spring-boot-application") version "9.4.0"
+
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.22"
+    kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "net.rcan"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2022.0.1"
+extra["springCloudVersion"] = "2023.0.0"
 dependencyManagement {
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
@@ -34,34 +37,35 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.springframework.cloud:spring-cloud-starter")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.cloud:spring-cloud-starter-consul-discovery")
 
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-tracing")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave")
 
-    // openapi - swagger
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.3")
-
+    // https://mvnrepository.com/artifact/org.springdoc/springdoc-openapi-starter-webmvc-ui
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.module/jackson-module-kotlin
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.0")
     // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
-    implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
-
-    runtimeOnly("com.mysql:mysql-connector-j")
-    runtimeOnly("com.h2database:h2")
-
-
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.0")
     implementation("com.thedeanda:lorem:1.2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
@@ -76,7 +80,7 @@ tasks.named("bootJar") {
 
 docker {
     springBootApplication {
-        baseImage.set("gradle:jdk17")
+        baseImage.set("amazoncorretto:21-alpine")
         ports.set(listOf())
         images.set(listOf("inhu00/sample-springboot3-kotlin:0.0.1","inhu00/sample-springboot3-kotlin:latest"))
     }
